5e5a89dff2faa8342b4111d741ba113e03fe55e9
andela-esitati <elsis.sitati@andela.com>
2016-09-18 19:49:13 +0300

    Add show_info method

--- inheritance.py
+++ inheritance.py
@@ -4,8 +4,12 @@ class Parent():
 		self.last_name = last_name
 		self.eye_color = eye_color
 
+	def show_info(self):
+		print "%s %s" %(self.last_name,self.eye_color)
+
 billy_cyrus = Parent("Cyrus","blue")
-print billy_cyrus.last_name
+billy_cyrus.show_info()
+
 
 class Child(Parent):
 	def __init__(self,last_name,eye_color,number_of_toys):
@@ -17,5 +21,6 @@ class Child(Parent):
 		self.number_of_toys = number_of_toys
 
 miley_cyrus = Child("Cyrus","blue",5)
-print miley_cyrus.last_name
-print miley_cyrus.number_of_toys
+miley_cyrus.show_info()
+# print miley_cyrus.last_name
+# print miley_cyrus.number_of_toys
